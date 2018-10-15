bc677d76d45a5309c5a0be144e4af44c4c113914
Sven Glöckner <sven.gloeckner@live.com>
2018-10-15 17:58:37 +0200

    Initial checkin

---
+++ HelloWorldDragDrop/HelloWorldDragDrop.App/Services/DragDropHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorldDragDrop.App.Services
+{
+    public static class DragDropHelper
+    {
+        public static DragItem[] Items = new[]
+        {
+            new DragItem
+            {
+                Text = "Drag Item 1",
+                Description = "foo",
+                Index  = 1
+            },
+            new DragItem
+            {
+                Text = "Drag Item 2",
+                Description = "bar",
+                Index  = 1
+            }
+        };
+
+        public static DragItem Item { get; set; }
+    }
+}
---
+++ HelloWorldDragDrop/HelloWorldDragDrop.App/Services/DragItem.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorldDragDrop.App.Services
+{
+    /// <summary>
+    /// This is a simple Drag drop data item.
+    /// </summary>
+    public class DragItem
+    {
+        public string Text { get; set; }
+
+        public string Description { get; set; }
+        public int Index { get; set; }
+    }
+}
+    
