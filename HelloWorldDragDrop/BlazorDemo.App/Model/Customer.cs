c0801d0195700d1ddbe94847e7ce859213385407
Sven Glöckner <sven.gloeckner@live.com>
2019-02-22 19:21:19 +0100

    Add DxSelectBox Demos

---
+++ HelloWorldDragDrop/BlazorDemo.App/Model/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorDemo.Model
+{
+    /// <summary>
+    /// This is a simple customer model.
+    /// </summary>
+    public class Customer
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Name2 { get; set; }
+
+        public string TaxNumber { get; set; }
+
+        public string Street { get; set; }
+    }
+}
--- HelloWorldDragDrop/README.md
+++ HelloWorldDragDrop/README.md
@@ -5,6 +5,8 @@ This example shows how to implement some features in a Blazor App:
   * Tested with Chrome and Edge
   * Currently does not work with Firefox due to some missing API bridges in Blazor
 * show a bootstrap popup
+* use DevExpress DevExtreme HTML widgets
+ 
 
 Live demo: https://blazordemo.azurewebsites.net/
 
