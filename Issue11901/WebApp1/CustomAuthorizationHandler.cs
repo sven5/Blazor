792dddc50a2430b68553333d98529b85119b3ea2
Sven Glöckner <sven.gloeckner@live.com>
2019-07-05 16:39:13 +0200

    Create test case

---
+++ Issue11901/WebApp1/CustomAuthorizationHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace WebApp1
+{
+    public class CustomAuthorizationHandler : AuthorizationHandler<CustomRequirement>
+    {
+        private readonly ILogger<CustomAuthorizationHandler> _logger;
+
+        public CustomAuthorizationHandler(ILogger<CustomAuthorizationHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomRequirement requirement)
+        {
+            _logger.LogDebug($"Principal's Identity is WindowsIdentity {context.User.Identity.Name}");
+            context.Succeed(requirement);
+            return Task.FromResult(0);
+        }
+    }
+}
